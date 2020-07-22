63fc9b8fced024b1d2746d4f47ab1721c64dbb78
reesebre82 <reesebre82@gmail.com>
2020-07-22 16:01:23 -0500

    Make changes for consistency. Changed parameters names to be the same and used 'this.'

--- EmployeeModel.java
+++ EmployeeModel.java
@@ -1,32 +1,35 @@
-package scheduler.model;
 
 public abstract class EmployeeModel implements Model {
   private String firstName;
   private String lastName;
   private int phoneNumber;
   private double salary;
+
   // private _ scheduleRestrictions;
   // private _ mySchedule; // Schedule class not implemented yet
   private int startDate[3];
   
-  public void setFirstName(String fName){
-    firstName = fName; }
-  
-  public void setLastName(String lName){
-    lastName = lName; }
-    
-  public void setPhoneNumber(int pNum){
-    phoneNumber = pNum; }
-  
-  public void setSalary(int sal){
-    salary = sal; }
-    
+  public void setFirstName(String firstName){
+    this.firstName = firstName; }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setPhoneNumber(int phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public void setSalary(int salary){
+    this.salary = salary; }
+
   // void setScheduleRestrictions(){}
   // void getMySchedule(){};
-    
-  public void setStartDate(int sDate[3]){
-  startDate[0] = sDate[0];
-  startDate[1] = sDate[1];
-  startDate[2] = sDate[2]; }
+
+  public void setStartDate(int startDate[3]){
+    this.startDate[0] = startDate[0];
+    this.startDate[1] = startDate[1];
+    this.startDate[2] = startDate[2]; 
+    }
   
 }
