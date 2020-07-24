d3feef4fee6194d7162428875ab5196b455c3762
reesebre82 <reesebre82@gmail.com>
2020-07-23 21:06:23 -0500

    Added constuctors and fixed syntax

--- EmployeeModel.java
+++ EmployeeModel.java
@@ -6,35 +6,58 @@ public abstract class EmployeeModel {
 
   // private _ scheduleRestrictions;
   // private _ mySchedule; // Schedule class not implemented yet
-  private int startDate[3];
-  
-  public void setFirstName(String firstName){
-    this.firstName = firstName; }
-  
-  public String getFirstName(){
-    return firstName; }
+  private int startDate[];
+
+  EmployeeModel() {
+    startDate = new int[3];
+    firstName = "";
+    lastName = "";
+    phoneNumber = 0;
+  }
+
+  EmployeeModel(String firstName, String lastName, int phoneNumber) {
+    startDate = new int[3];
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
 
   public void setLastName(String lastName) {
-    this.lastName = lastName; }
-  
-  public String getLastName(){
-    return lastName; }
+    this.lastName = lastName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
 
   public void setPhoneNumber(int phoneNumber) {
-    this.phoneNumber = phoneNumber; }
-  
-  public int getPhoneNumber(){
-    return phoneNumber; }
+    this.phoneNumber = phoneNumber;
+  }
 
-  // void setScheduleRestrictions(){} //Waiting on Schedule model to be implemented
+  public int getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  // void setScheduleRestrictions(){} //Waiting on Schedule model to be
+  // implemented
   // void getMySchedule(){}; //Waiting on Schedule model to be implemented
 
-  public void setStartDate(int sDate[3]){
+  public void setStartDate(int sDate[]) {
     this.startDate[0] = sDate[0];
     this.startDate[1] = sDate[1];
-    this.startDate[2] = sDate[2]; }
-  
-  public int[] getStartDate(){
-    return startDate; }
+    this.startDate[2] = sDate[2];
+  }
+
+  // public int[] getStartDate(){
+  // return startDate;
+  // }
 
 }
