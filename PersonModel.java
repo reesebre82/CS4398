6f6b59d10e4e61e6e01d7edb01afa65746526dca
reesebre82 <reesebre82@gmail.com>
2020-07-24 13:39:05 -0500

    Model for every person general attributes

---
+++ PersonModel.java
@@ -0,0 +1,29 @@
+public abstract class PersonModel {
+    protected String firstName;
+    protected String lastName;
+    protected String phoneNumber;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
