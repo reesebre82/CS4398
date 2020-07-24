61d9ec1100820e2abffe362673edd418759f7b80
reesebre82 <reesebre82@gmail.com>
2020-07-24 13:35:41 -0500

    update to add employees to database

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -2,20 +2,43 @@
 import java.sql.*;
 
 public class DatabaseManager {
+    private static String url = "jdbc:mysql://160.153.41.196/VetCenter";
+    private static String UUID = "F7i.2OPy$9.)=";
+    private static String deviceID = "fhfwtd6ujd8m";
 
-    static void AddEmployee() {
-        String url = "jdbc:mysql://160.153.41.196/VetCenter";
-        String UUID = "F7i.2OPy$9.)=";
-        String deviceID = "fhfwtd6ujd8m";
+    static int getIncrementalEID() {
         try {
-            String query = "insert into Employee (EmployeeID, FirstName, LastName, PhoneNumber) values (2, 'Joey', 'Reese', '5129000702')";
+            String query = "SELECT * FROM Employee ORDER BY EID DESC LIMIT 1";
 
             Connection conn = DriverManager.getConnection(url, deviceID, UUID);
             Statement statement = conn.createStatement();
 
-            statement.executeUpdate(query);
+            ResultSet rSet = statement.executeQuery(query);
 
-            System.out.println("Query Added");
+            while (rSet.next()) {
+                return rSet.getInt("EID") + 1;
+            }
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    static void AddEmployee(Employee employee) {
+        try {
+            String prepareStatement = "insert into Employee (EID, FirstName, LastName, PhoneNumber) values (?, ?, ?, ?)";
+
+            Connection conn = DriverManager.getConnection(url, deviceID, UUID);
+            PreparedStatement statement = conn.prepareStatement(prepareStatement);
+
+            statement.setInt(1, DatabaseManager.getIncrementalEID());
+            statement.setString(2, employee.getFirstName());
+            statement.setString(3, employee.getLastName());
+            statement.setString(4, employee.getPhoneNumber());
+
+            statement.executeUpdate();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -23,9 +46,6 @@ public class DatabaseManager {
     }
 
     static void PrintEmployees() {
-        String url = "jdbc:mysql://160.153.41.196/VetCenter";
-        String UUID = "F7i.2OPy$9.)=";
-        String deviceID = "fhfwtd6ujd8m";
         try {
             String query = "select * from Employee";
 
@@ -35,7 +55,7 @@ public class DatabaseManager {
             ResultSet rSet = statement.executeQuery(query);
 
             while (rSet.next()) {
-                System.out.print(rSet.getString("EmployeeID") + ": " + rSet.getString("FirstName"));
+                System.out.print(rSet.getString("EID") + ": " + rSet.getString("FirstName"));
                 System.out.println(" " + rSet.getString("LastName") + " " + rSet.getString("phoneNumber"));
             }
 
