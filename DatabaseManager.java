ae69792503a8d166f4ea88f67332eef56d82fec0
reesebre82 <reesebre82@gmail.com>
2020-07-23 22:48:45 -0500

    initial commit for Databasemanager

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -2,18 +2,41 @@
 import java.sql.*;
 
 public class DatabaseManager {
-    private String username = "CSUser";
-    private String password = "CS1234";
 
-    DatabaseManager() {
+    static void AddEmployee() {
+        String url = "jdbc:mysql://160.153.41.196/VetCenter";
+        String UUID = "F7i.2OPy$9.)=";
+        String deviceID = "fhfwtd6ujd8m";
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", username, password);
+            String query = "insert into Employee (EmployeeID, FirstName, LastName, PhoneNumber) values (2, 'Joey', 'Reese', '5129000702')";
+
+            Connection conn = DriverManager.getConnection(url, deviceID, UUID);
             Statement statement = conn.createStatement();
 
-            ResultSet rSet = statement.executeQuery("select * from Employee");
+            statement.executeUpdate(query);
+
+            System.out.println("Query Added");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void PrintEmployees() {
+        String url = "jdbc:mysql://160.153.41.196/VetCenter";
+        String UUID = "F7i.2OPy$9.)=";
+        String deviceID = "fhfwtd6ujd8m";
+        try {
+            String query = "select * from Employee";
+
+            Connection conn = DriverManager.getConnection(url, deviceID, UUID);
+            Statement statement = conn.createStatement();
+
+            ResultSet rSet = statement.executeQuery(query);
 
             while (rSet.next()) {
-                System.out.println(rSet.getString("EmployeeID") + ", " + rSet.getString("FirstName"));
+                System.out.print(rSet.getString("EmployeeID") + ": " + rSet.getString("FirstName"));
+                System.out.println(" " + rSet.getString("LastName") + " " + rSet.getString("phoneNumber"));
             }
 
         } catch (Exception e) {
