4e539cec166b7e43a63d364765602b016a6b778b
reesebre82 <reesebre82@gmail.com>
2020-07-24 16:27:46 -0500

    merged with branch :brendan

--- API/Customer.java
+++ API/Customer.java
@@ -5,5 +5,34 @@ import java.util.ArrayList;
 public class Customer extends PersonModel {
 
     ArrayList<Pet> pets;
+    int customerID;
 
+    public Customer() {
+        super();
+        customerID = -1;
+    }
+
+    public Customer(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        customerID = -1;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String toString() {
+        String str = "";
+        str += Integer.toString(customerID) + " ";
+        str += firstName + " ";
+        str += lastName + " ";
+        str += phoneNumber + " ";
+        return str;
+    }
 }
