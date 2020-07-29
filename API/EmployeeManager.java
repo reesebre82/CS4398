9b2c83c9e18e18242d8739c8b6b787ebf0a2c243
reesebre82 <reesebre82@gmail.com>
2020-07-29 15:48:08 -0500

    added EmployeeManager functionality

--- API/EmployeeManager.java
+++ API/EmployeeManager.java
@@ -2,21 +2,78 @@ package API;
 
 import java.util.ArrayList;
 
+import DatabaseSystems.DatabaseManager;
+
 public class EmployeeManager {
-    ArrayList<Employee> employees;
+    private ArrayList<Employee> employees;
 
-    // public EmployeeManager() Empty constructor that instantiates employees as
-    // empty arraylist or get data from database//brendan will implement this
+    public EmployeeManager() {
+        employees = DatabaseManager.getEmployeesFromDatabase();
+    }
 
-    // Employee getEmployee(String firstName, String lastName) function that returns
-    // a employee with a given first and last name
+    public void addEmployee(Employee employee) {
+        employee.setEmployeeID(DatabaseManager.addEmployee(employee));
+        employees.add(employee);
+    }
 
-    // Employee getEmployee(int customerID) function that returns a customer with a
-    // given Employee ID
+    public Employee getEmployee(String firstName, String lastName) {
+        for (Employee employee : employees)
+            if (firstName.equals(employee.getFirstName()) && lastName.equals(employee.getLastName()))
+                return employee;
 
-    // Void updateEmployee(int employeeID, Employee updatedEmployee) where it will
-    // update the employee with the same employeeID in employees(after completed,
-    // brendan will add a section to update database)
+        Employee employee = new Employee();
+        employee.setFirstName("ERROR");
+        employee.setLastName("ERROR");
+        return employee;
+    }
 
-    // deconstructor to push data to database //Brendan will implement this;
+    public Employee getCustomer(int employeeID) {
+        for (Employee employee : employees)
+            if (employeeID == employee.getEmployeeID())
+                return employee;
+
+        Employee employee = new Employee();
+        employee.setFirstName("ERROR");
+        employee.setLastName("ERROR");
+        return employee;
+    }
+
+    public void updateEmployee(int employeeID, Employee updatedEmployee) {
+        for (Employee employee : employees) {
+            if (employeeID == employee.getEmployeeID()) {
+                updatedEmployee.setEmployeeID(employeeID);
+                employees.remove(employee);
+                employees.add(updatedEmployee);
+                DatabaseManager.updateEmployee(employee);
+            }
+        }
+    }
+
+    public void closeDatabase() {
+        for (Employee employee : employees)
+            DatabaseManager.updateEmployee(employee);
+    }
+
+    public String toString() {
+        String str = "Employee Data: \n";
+        int index = 1;
+        for (Employee employee : employees) {
+            str += index + ".\n\t";
+            str += "EID: ";
+            str += employee.getEmployeeID() + "\n\t";
+            str += "First Name: ";
+            str += employee.getFirstName() + "\n\t";
+            str += "Last Name: ";
+            str += employee.getLastName() + "\n\t";
+            str += "Phone Number: ";
+            str += employee.getPhoneNumber() + "\n\t";
+            str += "Start Date: ";
+            str += employee.getStartDate() + "\n\t";
+            str += "\n\n";
+
+            // Add information for when schedules are created
+            index++;
+        }
+        return str;
+    }
 }
