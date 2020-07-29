f5470c3a5936e04fa6b6d1387e36c997933974f3
reesebre82 <reesebre82@gmail.com>
2020-07-29 01:57:07 -0500

    First implementation for managing the cusotmers

--- API/CustomerManager.java
+++ API/CustomerManager.java
@@ -1,8 +1,60 @@
 package API;
 
 import java.util.ArrayList;
+import DatabaseSystems.*;
 
 public class CustomerManager {
     ArrayList<Customer> customers;
 
+    public CustomerManager() {
+        customers = DatabaseManager.getCustomersFromDatabase();
+
+        ArrayList<Pet> pets = DatabaseManager.getPetsFromDatabase();
+
+        for (Pet pet : pets) {
+            for (Customer customer : customers) {
+                if (customer.getCustomerID() == pet.getCustomerID())
+                    customer.addPet(pet);
+            }
+        }
+    }
+
+    public Customer getCustomer(String firstName, String lastName) {
+        for (Customer customer : customers)
+            if (firstName.equals(customer.getFirstName()) && lastName.equals(customer.getLastName()))
+                return customer;
+
+        Customer customer = new Customer();
+        customer.setFirstName("ERROR");
+        customer.setLastName("ERROR");
+        return new Customer();
+    }
+
+    public Customer getCustomer(int customerID) {
+        for (Customer customer : customers)
+            if (customerID == customer.getCustomerID())
+                return customer;
+
+        Customer customer = new Customer();
+        customer.setFirstName("ERROR");
+        customer.setLastName("ERROR");
+        return new Customer();
+    }
+
+    public void updateCustomer(int customerID, Customer updatedCustomer) {
+        for (Customer customer : customers) {
+            if (customerID == customer.getCustomerID()) {
+                updatedCustomer.setCustomerID(customerID);
+                customers.remove(customer);
+                customers.add(updatedCustomer);
+                DatabaseManager.updateCustomer(updatedCustomer);
+            }
+        }
+    }
+
+    public void closeDatabase() {
+        for (Customer customer : customers) {
+            DatabaseManager.updateCustomer(customer);
+        }
+    }
 }
