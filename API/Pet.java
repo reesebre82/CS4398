80f090609f4320e54661740b4cd4d307e1d3777b
reesebre82 <reesebre82@gmail.com>
2020-07-29 00:15:55 -0500

    added pet class

--- API/Pet.java
+++ API/Pet.java
@@ -2,8 +2,98 @@ package API;
 
 public class Pet {
 
-    // http://brendanreese.com/Images/Pet.png
-    // Implement a class similar to Customer that fills out the details shown with
-    // the link Dont extend PersonModel
+    int petID;
+    int customerID;
+    String name;
+    String species;
+    String breed;
+    String birthday;
+    String gender;
 
+    public Pet() {
+        petID = customerID = -1;
+        name = species = breed = gender = birthday = "";
+    }
+
+    public Pet(int petID, int customerID, String name, String species, String breed, String birthday, String gender) {
+        this.petID = petID;
+        this.customerID = customerID;
+        this.name = name;
+        this.species = species;
+        this.breed = breed;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+
+    public String toString() {
+        String str = "";
+        str += Integer.toString(petID) + " ";
+        str += Integer.toString(customerID) + " ";
+        str += name + " ";
+        str += species + " ";
+        str += breed + " ";
+        str += birthday + " ";
+        str += gender;
+        return str;
+    }
+
+    /* Start of Getters and Setters */
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPetID(int petID) {
+        this.petID = petID;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getPetID() {
+        return petID;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    /* End of Getters and Setters */
 }
