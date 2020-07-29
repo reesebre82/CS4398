bdae35e7bca19250d81dde5ebc8d3ed98b7a87c3
reesebre82 <reesebre82@gmail.com>
2020-07-29 00:13:18 -0500

    made functions public

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -12,7 +12,7 @@ public class GUI {
     private static GraphicsConfiguration gc;
     private JFrame frame;
 
-    GUI() {
+    public GUI() {
         frame = new JFrame(gc);
         frame.setSize(800, 600);
         frame.setLocation(300, 200);
@@ -20,7 +20,7 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    void addBackground() {
+    public void addBackground() {
 
         try {
             URL url = new URL("https://animalcarecentersmyrna.com/wp-content/uploads/2016/11/pets_big.png");
@@ -42,7 +42,7 @@ public class GUI {
         frame.setVisible(true);
     }
 
-    void addButtons() {
+    public void addButtons() {
 
     }
 
