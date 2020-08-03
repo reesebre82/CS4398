b21ac936cc7678a1eb8db3a1c2c41669f037c934
reesebre82 <reesebre82@gmail.com>
2020-08-02 20:44:34 -0500

    small changes for testing

--- VetApp.java
+++ VetApp.java
@@ -1,13 +1,25 @@
 
+import java.util.ArrayList;
+
 import API.*;
+import DatabaseSystems.DatabaseManager;
 import GUI.*;
+import SchedulerSystem.*;
 
 public class VetApp {
     public static void main(String[] args) {
 
-        CustomerManager cm = new CustomerManager();
-        EmployeeManager em = new EmployeeManager();
-        System.out.println(cm);
-        System.out.println(em);
+        // CustomerManager cm = new CustomerManager();
+        // EmployeeManager em = new EmployeeManager();
+        // System.out.println(cm);
+        // System.out.println(em);
+
+        // GUI gui = new GUI();
+
+        Scheduler scheduler = new Scheduler();
+        ArrayList<Employee> employees = DatabaseManager.getEmployeesFromDatabase();
+        scheduler.PopulateWeek(employees);
+
+        System.out.println("ended");
     }
 }
